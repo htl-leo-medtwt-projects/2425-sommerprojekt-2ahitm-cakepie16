a83473ffc82b1ffb899ff961f22539879f37496a
Sophie Kempl <s.kempl@students.htl-leonding.ac.at>
2025-04-29 10:53:29 +0200

    @media, Einkaufsliste anfang

--- project/script/script.js
+++ project/script/script.js
@@ -3,6 +3,7 @@ function showOOEProducts() {
   for (let i = 0; i < ooeGaertnerProdukte.length; i++) {
     let category = `${ooeGaertnerProdukte[i].kategorie[0]}, ${ooeGaertnerProdukte[i].kategorie[0]}`;
     output += `<div class="product">
+      <i class="fa fa-shopping-cart cart-icon" onclick="addItemToList('${ooeGaertnerProdukte[i].name}')"></i>
       <img src="${ooeGaertnerProdukte[i].img}" alt="OÖ Gärtner Produkt">
       <h2>${ooeGaertnerProdukte[i].name}</h2>
       <p>${category}</p>
@@ -11,6 +12,7 @@ function showOOEProducts() {
   }
   document.getElementById('products').innerHTML = output
 }
+showArray('herbs')
 function showArray(category) {
   let result = [];
 
@@ -39,7 +41,7 @@ function showArray(category) {
   for (let i = 0; i < result.length; i++) {
     output += `
     <div class="product">
-      <i class="fa fa-shopping-cart cart-icon"></i>
+      <i class="fa fa-shopping-cart cart-icon" onclick="addItemToList('${result[i].name}')"></i>
       <img src="${result[i].img}">
       <h2>${result[i].name}</h2>
       <!--${result[i].beschreibung}-->
@@ -47,4 +49,27 @@ function showArray(category) {
     
   }
   document.getElementById('products').innerHTML = output
+}
+
+function addItemToList(productName) {
+  let shoppingList = JSON.parse(localStorage.getItem('products')) || [];
+
+  if (!shoppingList.includes(productName)) {
+    shoppingList.push(productName);
+    localStorage.setItem('products', JSON.stringify(shoppingList));
+    console.log(`Produkt "${productName}" wurde zur Einkaufsliste hinzugefügt.`);
+  } else {
+    console.log(`Produkt "${productName}" ist bereits in der Einkaufsliste.`);
+  }
+
+}
+function showShoppingList() {
+  let shoppingList = JSON.parse(localStorage.getItem('products')) || [];
+  let output = "";
+
+  shoppingList.forEach(item => {
+    output += `<div>${item}</div>`;
+  });
+
+  document.getElementById('products').innerHTML = output;
 }
