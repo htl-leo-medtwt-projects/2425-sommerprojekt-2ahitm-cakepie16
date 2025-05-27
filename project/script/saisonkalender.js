c4facd8ff0fc054055b389ea39f18e66ca233253
Sophie Kempl <s.kempl@students.htl-leonding.ac.at>
2025-05-27 17:38:59 +0200

    Saisonkalender

--- project/script/saisonkalender.js
+++ project/script/saisonkalender.js
@@ -1,8 +1,5 @@
 let months = [
-    "Jänner", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember" 
-]
-let months_letter = [
-    "J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D" 
+    "Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez" 
 ]
 let category = [
   {
@@ -40,7 +37,7 @@ function loadMonths() {
     for (let i = 0; i < months.length;i++) {
         month += `
         <div class="month" id="month${i}"  onclick="showMonth(${i})">
-            <p>${months_letter[i]}</p>
+            <p>${months[i]}</p>
         </div>`;
     }
     document.getElementById('months').innerHTML = month;
